d0f3f984dc7294f8871b226eabf9f2114054fbf3
Verda Bursal <verdabursal@Verdas-MacBook-Air.local>
2019-05-30 11:40:11 -0400

    created main page components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,42 @@ import './App.css';
 
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+import Home from './components/Home';
+import MyCollection from './components/MyCollection';
+import Favorites from './components/Favorites';
+import Add from './components/Add';
+import Settings from './components/Settings';
+
 class App extends React.Component {
   render() {
     return(
         <Router>
+            <Link to="/">Home</Link>&nbsp;
+            <Link to="/my-collection">My Collection</Link>&nbsp;
+            <Link to="/favorites">Favorites</Link>&nbsp;
+            <Link to="/add">Add</Link>&nbsp;
+            <Link to="/settings">Settings</Link>
 
+            <Route
+                path="/"
+                exact
+                component={Home}/>
+            <Route
+                path="/my-collection"
+                exact
+                component={MyCollection}/>
+            <Route
+                path="/favorites"
+                exact
+                component={Favorites}/>
+            <Route
+                path="/add"
+                exact
+                component={Add}/>
+            <Route
+                path="/settings"
+                exact
+                component={Settings}/>
         </Router>
     );
   }
