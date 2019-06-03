6f4b0fe913c114ca50d7107f32d64f6dd6ae7725
Verda Bursal <verdabursal@Verdas-MacBook-Air.local>
2019-06-03 13:48:50 -0400

    made My Collection table, populated with mock data

--- src/components/MyCollection.jsx
+++ src/components/MyCollection.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 
+import mockData from '../mock-data/collection.mock';
+
 class MyCollection extends React.Component {
     render() {
         return(
             <div>
                 <h1>My Collection</h1>
+                <table className="table">
+                    <tbody>
+                    <tr>
+                        <th>{"ID"}</th>
+                        <th>{"Category"}</th>
+                        <th>{"Type"}</th>
+                        <th>{"Year"}</th>
+                        <th>{"Region"}</th>
+                        <th>{"Label"}</th>
+                        <th>{"Description"}</th>
+                    </tr>
+                    {
+                        mockData.map(
+                            bottle =>
+                                <tr key={bottle.id}>
+                                    <td>{bottle.id}</td>
+                                    <td>{bottle.category}</td>
+                                    <td>{bottle.type}</td>
+                                    <td>{bottle.year}</td>
+                                    <td>{bottle.region}</td>
+                                    <td>{bottle.label}</td>
+                                    <td>{bottle.description}</td>
+                                </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
             </div>
         )
     }
