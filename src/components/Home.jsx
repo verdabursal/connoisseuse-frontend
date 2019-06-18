b65fb13a7c2ae04f864ee8ddfbe0f09db4ba9de1
Verda Bursal <verdabursal@Verdas-MacBook-Air.local>
2019-06-18 00:03:27 -0400

    added functions in Home for setting form username and password; created config file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 
-import {BrowserRouter as Router, Link} from "react-router-dom";
-
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: props.username
+            username: props.username,
+            formUsername: "",
+            formPassword: ""
         }
     }
 
-    async componentDidMount() {
-        console.log(this.props)
-    }
+    setFormUsername = async event => {
+        await this.setState({ formUsername: event.target.value });
+        console.log("set username: ");
+        console.log(this.state.formUsername);
+    };
 
+    setFormPassword = async event => {
+        await this.setState({ formPassword: event.target.value });
+        console.log("set password: ");
+        console.log(this.state.formPassword);
+    };
+
+    logIn = async () => {
+
+    };
+
+    signUp = async () => {
+
+    };
 
     render() {
         if (this.state.username == null) {
@@ -23,14 +38,20 @@ class Home extends React.Component {
                     <br/>
                     <div className="form-group">
                         <label htmlFor="inputUsername">Username</label>
-                        <input type="text" className="form-control" id="inputUsername"/>
+                        <input type="text" className="form-control" id="inputUsername"
+                        onChange={event => this.setFormUsername(event)}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputPassword">Password</label>
-                        <input type="password" className="form-control" id="inputPassword"/>
+                        <input type="password" className="form-control" id="inputPassword"
+                        onChange={event => this.setFormPassword(event)}/>
                     </div>
-                    <button className="btn btn-primary">Log In</button>&nbsp;
-                    <button className="btn btn-secondary">Sign Up</button>
+                    <button className="btn btn-primary" onClick={this.logIn}>
+                        Log In
+                    </button>&nbsp;
+                    <button className="btn btn-secondary" onClick={this.signUp}>
+                        Sign Up
+                    </button>
                 </div>
             )
         } else {
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    backendAPI: 'http://localhost:8080/api'
+};
+
+export default config;
